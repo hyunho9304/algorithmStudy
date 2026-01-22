abdc221d522e14a43358fd0c6baba58790c3d112
hyunho <hyunho9304@gmail.com>
2026-01-22 17:28:05 +0900

    260122

---
+++ define/Chap.4/b.응용.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+const int max_n = 4 ;
+string a[max_n] = { "사과", "딸기", "포도", "배" } ;
+
+
+void go( int num ) {
+    
+    string ret = "" ;
+    for( int i = 0 ; i < 4 ; i++ ) {
+        if( num & ( 1 << i ) ) ret += a[i] + " " ;
+    }
+    cout << ret << "\n" ;
+}
+
+int main() {
+    
+    for( int i = 0 ; i < ( 1 << max_n ) ; i++ ) {
+        string ret = "" ;
+        for( int j = 0 ; j < max_n ; j++ ) {
+            if( i & ( 1 << j ) ) {
+                ret += ( a[j] + " " ) ;
+            }
+        }
+        cout << ret << "\n" ;
+    }
+
+    
+    cout << "\n" ;
+//  ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+    cout << "\n" ;
+
+
+    for( int i = 0 ; i < max_n ; i++ ) {
+        go( 1 | ( 1 << i ) ) ;
+    }
+
+    return 0 ;
+
+
+}
